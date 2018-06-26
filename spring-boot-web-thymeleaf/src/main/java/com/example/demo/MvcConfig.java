ca50ac131de82ee4c2cd542fd91c7a43e22c10d7
Nabil Ouerhani <n.ouerhani@gmail.com>
2018-06-26 13:51:17 +0200

    With MvcConfig

--- spring-boot-web-thymeleaf/src/main/java/com/example/demo/MvcConfig.java
+++ spring-boot-web-thymeleaf/src/main/java/com/example/demo/MvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+	//MVC config: mapping between URL patterns and html files
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
