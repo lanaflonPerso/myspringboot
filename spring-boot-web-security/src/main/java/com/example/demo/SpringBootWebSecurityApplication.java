359c67a3c94308563d26d5c449f6b355983d0238
Nabil Ouerhani <n.ouerhani@gmail.com>
2018-06-26 14:24:03 +0200

    spring-boot-web-security: basic security

---
+++ spring-boot-web-security/src/main/java/com/example/demo/SpringBootWebSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSecurityApplication.class, args);
+	}
+}
